594b27b590f20a8c95d74df71b0e1ed8330fe707
Antonio Hernández <antonio@civio.es>
2022-05-06 09:02:41 +0200

    Add vue-cli example

---
+++ Javascript/vue-3-introduction/vue-cli/README.md
@@ -0,0 +1,36 @@
+# vue-cli
+
+## Before the next steps
+
+Install globally vue-cli
+```
+npm install -g @vue/cli
+```
+
+Create Vue project inside directory
+```
+vue create ./
+```
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ Javascript/vue-3-introduction/vue-cli/src/components/LocationItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="place">
+    <img :src="location.img"/>
+    <header>
+      <slot name="header"></slot>
+      <h2>{{location.name}}</h2>
+    </header>
+    <p>{{location.desc}}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "LocationItem",
+  props: ["location"],
+}
+</script>
+<style>
+.place {
+  display: flex;
+  flex-direction: column;
+  width: 280px;
+  justify-content: center;
+  background: white;
+  border: 1px solid #ddd;
+  padding: 20px 20px;
+  margin: 10px;
+  h2 {
+    margin: 0;
+    text-align: center;
+  }
+  img {
+    margin: 10px;
+    align-self: center;
+  }
+}
+</style>
