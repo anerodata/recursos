5d3a0826f31fc8a42a6c50c6df7c6fbcb1860a60
anerodata <antonio92.ahr@gmail.com>
2022-05-07 12:26:25 +0200

    Feed state

--- Javascript/vue-3-introduction/vue-store-state/src/App.vue
+++ Javascript/vue-3-introduction/vue-store-state/src/App.vue
@@ -28,24 +28,8 @@ export default {
   },
   data() {
     return {
-      newComment: '',
-      comments: [
-        {
-          text: 'Looks great Julianne!',
-          author: 'Robin Rendle',
-          authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-coffee.jpg'
-        },
-        {
-          text: 'I love the Sea',
-          author: 'Miriam Suzanne',
-          authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-miriam.jpg'
-        },
-        {
-          text: 'Where are you?',
-          author: 'Geoff Graham',
-          authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-geoff.jpg'
-        }
-      ]
+      comments: this.$store.state.comments,
+      newComment: this.$store.state.newComment
     }
   },
   methods: {
--- Javascript/vue-3-introduction/vue-store-state/src/store/index.js
+++ Javascript/vue-3-introduction/vue-store-state/src/store/index.js
@@ -2,6 +2,24 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    newComment: '',
+    comments: [
+      {
+        text: 'Looks great Julianne!',
+        author: 'Robin Rendle',
+        authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-coffee.jpg'
+      },
+      {
+        text: 'I love the Sea',
+        author: 'Miriam Suzanne',
+        authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-miriam.jpg'
+      },
+      {
+        text: 'Where are you?',
+        author: 'Geoff Graham',
+        authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-geoff.jpg'
+      }
+    ]
   },
   getters: {
   },
